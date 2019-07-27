aedf70608373fd5ebf631852c71e4c79e899a0b2
David Kurzmann <david.g.kurzmann@gmail.com>
2019-07-27 11:58:07 +0200

    initial commit

---
+++ DavidKurzmann.de/app.js
@@ -0,0 +1,11 @@
+function toggleNavigation(){
+    const navigation = 
+    document.getElementById('mainNavigation');
+
+    if(Element.classList.contains
+        ('isExpanded'){
+            navigation.classList.remove('isExpanded')
+        } else {
+            navigation.classList.add('isExpanded')
+        }
+}
